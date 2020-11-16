d6f696cbd8632862573d12f608a38654b59fd1f1
MaduFernandes <eduarda.alves.siqueira.fernandes@gmail.com>
2020-11-16 19:53:04 -0300

    feat: add file to messages

---
+++ src/main/java/br/com/oat/config/Message.java
@@ -0,0 +1,11 @@
+package br.com.oat.config;
+
+public final class Message {
+
+    public static final String CADASTRO = "Aluno cadastrado com sucesso";
+    public static final String ATUALIZACAO = "Aluno atualizado com sucesso";
+    public static final String DELETAR = "Aluno deletado com sucesso";
+
+    public Message() {
+    }
+}
